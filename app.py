d44547a750bdb2cc0fddbe5192caf5a11cc893d5
Thamalu <thamalumaliththa@gmail.com>
2020-09-01 14:47:22 +0530

    Add test 2

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return jsonify({"message":"This is our first test", "additional":"this is really cool"})
+
+@app.route('/get',methods=['GET'])
+def get():
+    json = request.args
+    result = ""
+    for i in json:
+        result += f"{i} is {json[i]}. \n"
+    return result
+
+@app.route('/post',methods=['POST'])
+def post():
+    json = request.json
+    result = ""
+    for i in json:
+        result += f"{i} is {json[i]}. \n"
+    return result
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
