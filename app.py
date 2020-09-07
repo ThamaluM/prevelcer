8222d1bdf0c5850d976ef4b36b931791b950245d
Thamalu <thamalumaliththa@gmail.com>
2020-09-07 15:34:53 +0530

    Added Last Names

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ''' <html> <h1>Ulcer prevention smart mattress</h1> <p>This is final year project conducted by </p><ul><li>Adrian</li><li>Buddhini</li><li>Helik</li><li>Thamalu</li></ul> </html>  '''
+    return ''' <html> <h1>Ulcer prevention smart mattress</h1> <p>This is final year project conducted by </p><ul><li>Adrian Perera</li><li>Buddhini Edirisinghe</li><li>Helik Tribuwan</li><li>Thamalu Piyadigama</li></ul> </html>  '''
     #return jsonify({"message":"This is our first test", "additional":"this is really cool"})
 
 @app.route('/get',methods=['GET'])
