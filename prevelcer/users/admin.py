b868d8cc2f611b84c4cc48b9d616707564588e47
Thamalu <thamalumaliththa@gmail.com>
2021-03-01 19:37:08 +0530

    Site profile in admin panel

--- prevelcer/users/admin.py
+++ prevelcer/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Profile, ProfileAdmin)
