fd9fb992baf3bb3bdd3ad4029176f81fa5e2ad0f
Thamalu <thamalumaliththa@gmail.com>
2021-04-14 00:29:58 +0530

    Remove buggy icon of notification

--- prevelcer/prevelcer/views.py
+++ prevelcer/prevelcer/views.py
@@ -5,13 +5,13 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello Welcome to our home page!</h1> - Thamalu, Helik, Buddhini, Adrian.") #"Hello Welcome"
+    return HttpResponse("<head><style>body {background-image: url('https://cdn.pixabay.com/photo/2016/11/18/12/06/white-male-1834117_960_720.jpg');}</style></head><html><body><center><h1>Hello Welcome to our home page!</h1> - Thamalu, Helik, Buddhini, Adrian.</center></body></html>") #"Hello Welcome"
 
 
 
 def send_notification(request):
     user = User.objects.get(username=request.GET["username"])
     device = FCMDevice.objects.filter(user=user).first()
-    device.send_message(request.GET["title"], request.GET["message"],icon=request.user.profile.picture.url)
+    device.send_message(request.GET["title"], request.GET["message"])
     return HttpResponse("Success")
     
